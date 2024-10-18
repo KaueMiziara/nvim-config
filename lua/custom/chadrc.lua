4c665b990f0793754708fed22a30f4e2bf656c59
KaueMiziara <kauemiziara@gmail.com>
2024-10-17 21:31:29 -0300

    Add transparency

--- lua/custom/chadrc.lua
+++ lua/custom/chadrc.lua
@@ -1,7 +1,10 @@
 ---@type ChadrcConfig
 local M = {}
 
-M.ui = { theme = 'mountain' }
+M.ui = {
+    theme = 'mountain',
+    transparency = true,
+}
 M.plugins = 'custom.plugins'
 M.mappings = require("custom.mappings")
 
