08ed101864dd435d391ec2ef7664a2da34254814
Kaue Miziara <kauemiziara@gmail.com>
2025-05-15 08:47:59 -0300

    Configure tab size to 4 spaces

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,3 +6,16 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+-- Identation
+local js_ts_indent = vim.api.nvim_create_augroup("JavaScriptTypeScriptIndent", { clear = true })
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "javascript", "typescript", "javascriptreact", "typescriptreact" },
+  callback = function()
+    vim.opt_local.tabstop = 4
+    vim.opt_local.shiftwidth = 4
+    vim.opt_local.softtabstop = 4
+    vim.opt_local.expandtab = true
+  end,
+  group = js_ts_indent,
+})
